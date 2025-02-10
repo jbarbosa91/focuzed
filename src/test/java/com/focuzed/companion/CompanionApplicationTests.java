6b6d3514dba770874a900c3c54e8b433b43155e9
José Barbosa <jbarbosa47@gmail.com>
2025-02-10 13:37:23 +0000

    First commit

---
+++ src/test/java/com/focuzed/companion/CompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focuzed.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
